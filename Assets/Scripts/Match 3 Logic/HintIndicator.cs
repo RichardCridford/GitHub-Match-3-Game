bad071498e3c8277fb9c255ef104374916ae7cef
Richard Cridford <r.e.cridford@gmail.com>
2025-08-18 16:34:03 +0200

    Time delay before being able to access the hint button.

--- Assets/Scripts/Match 3 Logic/HintIndicator.cs
+++ Assets/Scripts/Match 3 Logic/HintIndicator.cs
@@ -3,32 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/*
+ * This script allows displaying a sprite over a transform
+ * either when a button is pressed or automatically after a time dely
+ * or enabling the button after a time delay
+ * 
+ * This is a singleton and requires a SpriteRenderer component
+ */
+
+
+
 [RequireComponent(typeof(SpriteRenderer))]
 public class HintIndicator : Singleton<HintIndicator>
 {
+    // the sprite renderer component attached to a gameobject
     private SpriteRenderer spriteRenderer;
 
+    // where the hint will be displayed
     private Transform hintLocation;
 
+    // the coroutine that that delays before autohint
     private Coroutine autoHintCR;
 
+    // UI button component that displays a hint when pressed
     [SerializeField]
     private Button hintButton;
 
+    
+    // how many seconds should we wait before offering a hint
     [SerializeField]
     private float delayBeforeAutoHint;
 
+    // disable by default get sprite renderer
     protected override void Init()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.enabled = false;
         hintButton.interactable = false;
     }
+    // force the hint to dispaly on desired transform
     public void IndicateHint(Transform hintLocation)
     {
         transform.position = hintLocation.position;
         spriteRenderer.enabled = true;
     }
+    // remove the hint and interrupt whatever coroutine might be running
     public void CancelHint()
     {
         spriteRenderer.enabled = false;
@@ -39,11 +58,13 @@ public class HintIndicator : Singleton<HintIndicator>
         autoHintCR = null;
 
     }
+    // enable the hint button
     public void EnableHintButton()
     {
         hintButton.interactable = true;
 
     }
+    // start a coroutine that will wait before offering the hint
     public void StartAutoHint(Transform hintLocation)
     { 
         this.hintLocation = hintLocation;
@@ -51,10 +72,13 @@ public class HintIndicator : Singleton<HintIndicator>
         autoHintCR = StartCoroutine(WaitAndIndicateHint());
     
     }
+    // the coroutine that waits before dispalying the hint
     private IEnumerator WaitAndIndicateHint()
     {
         yield return new WaitForSeconds(delayBeforeAutoHint);
-        IndicateHint(hintLocation);
+        EnableHintButton();
+        
+        //IndicateHint(hintLocation);
     }
 
 
