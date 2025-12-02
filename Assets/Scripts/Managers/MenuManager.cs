a3ff8ffc9ae79628591104abde8be3b0d98341c1
Richard Cridford <r.e.cridford@gmail.com>
2025-12-02 22:53:07 +0100

    Added button graphics on main menu, started main menu manager script - need to finish code for loading scenes.

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField]
+    private Fader loadingScreen;
+
+    private void Start()
+    {
+        loadingScreen.Hide(false);
+        StartCoroutine(loadingScreen.Fade(0));
+    }
+
+    private IEnumerator Quit()
+    {
+        yield return StartCoroutine(loadingScreen.Fade(1));
+        Application.Quit();
+    }
+
+    private IEnumerator StartSurvivalGame()
+    {
+        yield return StartCoroutine(loadingScreen.Fade(1));
+        
+    }
+
+    private IEnumerator StartTimeRushGame()
+    {
+        yield return StartCoroutine(loadingScreen.Fade(1));
+       
+    }
+
+    public void QuitButtonPressed()
+    {
+        StartCoroutine(Quit());
+    
+    }
+
+    public void SurvivalButtonPressed()
+    {
+
+        StartCoroutine(StartSurvivalGame());
+    }
+
+    public void TimedRushButtonPressed()
+    {
+        StartCoroutine(StartTimeRushGame());
+
+    }
+}
