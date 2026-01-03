c5a50106fecbc13bb8e9421d40e70d4be6dc2c41
Richard Cridford <r.e.cridford@gmail.com>
2026-01-03 20:47:26 +0100

    Added code for Level Timer need to make sure Game Manager script works in both level types/

--- Assets/Scripts/Managers/LevelTimer.cs
+++ Assets/Scripts/Managers/LevelTimer.cs
@@ -5,5 +5,52 @@ using UnityEngine.UI;
 
 public class LevelTimer : MonoBehaviour
 {
+    private GameManager gm;
+    
+    
+    [SerializeField] 
+    private Text timerText;
+
+    private float timeRemainging;
+    private string timeAsString;
+
+    private bool counting;
+
+    private void Start()
+    {
+        gm = GameManager.Instance;
+    }
+
+
+
+    public void SetTimer(float t)
+    {
+        StopAllCoroutines();
+        timeRemainging = t;
+        UpdateText();
+    }
+
+    private void UpdateText()
+    {
+        timeAsString = (int)timeRemainging / 60 + " : ";
+        timeAsString += timeRemainging % 60 < 10 ? "0" : "";
+        timerText.text = timeAsString + (int) timeRemainging % 60;
+    }
+
+    public IEnumerator Countdown()
+    { 
+        counting = true;
+        do
+        { 
+            timeRemainging -= Time.deltaTime;
+            UpdateText() ;
+            yield return null;
+        }
+        while (timeRemainging > 0);
+
+        counting = false;
+
+        gm.GameOver();
+    }
 
 }
